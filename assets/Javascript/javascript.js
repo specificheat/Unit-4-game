6835ce3172bedf11aabfff15bab284842188fac2
Jeremy Pena <jpena.boots@gmail.com>
2020-05-01 20:56:09 -0700

    added js win and lose broken

--- assets/Javascript/javascript.js
+++ assets/Javascript/javascript.js
@@ -9,6 +9,98 @@ var crystals = [];
 for(var c = 1; c < 13; c++) {
     crystals.push(c);
 }
+var randNumber;
+var crystalNumbers = [];
+
+var c1;
+var c2;
+var c3;
+var c4;
+
+var totalScore = 0;
+
+var wins = 0;
+var losses = 0;
+
+
+function pickRandomNumber(arr){
+    var x = arr[Math.floor(Math.random() * arr.length)];
+    randNumber = x;
+    $("#random-number").html("Match: " + randNumber);
+    console.log("Random number: " + randNumber);
+}
+function pickRandomCrystals(arr){
+    for(var y = 0; y < 4; y++){
+        var a = arr[Math.floor(Math.random() * arr.length)];
+        crystalNumbers.push(a);
+    }
+    console.log("crystal numbers: " + crystalNumbers);
+}
+
+function crystalValues(arr){
+    for(i = 0; i < arr.length; i++){
+        $("#button-" + (i+1)).attr("value", arr[i]);
+        console.log(this);
+    
+    c1 = arr[0]
+    c2 = arr[1]
+    c3 = arr[2]
+    c4 = arr[3]
+    }    
+}
+
+    function gameReset(x){
+        crystalNumbers = [];
+        pickRandomNumber(rand);
+        pickRandomCrystals(crystals);
+        crystalValues(crystalNumbers);
+
+        totalScore = 0;
+        $("#points").html(totalScore);
+        alert(x);
+    }
+
+    pickRandomNumber(rand);
+    pickRandomCrystals(crystals);
+    crystalValues(crystalNumbers);
+
+    $("#button-1").on("click", function() {
+        totalScore += c1;
+        $("#points").html("Crystal points: " + totalScore);
+    });
+    $("#button-2").on("click", function() {
+        totalScore += c2;
+        $("#points").html("Crystal points: " + totalScore);
+
+    });
+    $("#button-3").on("click", function() {
+        totalScore += c3;
+        $("#points").html("Crystal points: " + totalScore);
+    });
+
+    $("#button-4").on("click", function(){
+        totalScore += c4;
+        $("#points").html("Crystal points: " + totalScore);
+    });
+    $("#button").on("click", function() {
+        if(totalScore === randNumber){
+            wins++;
+            console.log(totalScore);
+            $("#points").html(totalScore);
+            $("#wins").html("wins:" + wins);
+            setTimeout(function() {gameReset("Winner!")}, 20);
+        }
+        else if (totalScore > randNumber){
+            losses++
+            $("#points").html(totalScore);
+            $("#losses").html("Losses:" + losses);
+            setTimeout(function() {gameReset("Sadness....")}, 20);
+        }
+    
+    
+    })
+    
+    
 
 
 
@@ -18,5 +110,4 @@ for(var c = 1; c < 13; c++) {
 
 
 
-
-})
+});
