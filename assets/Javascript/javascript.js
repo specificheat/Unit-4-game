d768b8d1c02ad24dfe32d4f9b805f8c2c41827e1
Jeremy Pena <jpena.boots@gmail.com>
2020-05-01 17:39:56 -0700

    added most html and css

---
+++ assets/Javascript/javascript.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+var rand = [];
+
+for(var r = 19; r < 121; r++){
+    rand.push(r);
+}
+var crystals = [];
+
+for(var c = 1; c < 13; c++) {
+    crystals.push(c);
+}
+
+
+
+
+
+
+
+
+
+
+})
